58572d6921215dd731a8e589a8664c82ebf43fc3
Suhail P K <suhail.pk@epixelsolutions.com>
2024-11-05 17:37:39 +0530

    first commit

---
+++ backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('project', ProjectViewSet, basename='project')
+urlpatterns = router.urls
+
+
